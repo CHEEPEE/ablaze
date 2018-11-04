0a86213e6405cecab3b2938c672cc3ee96d615bd
Unknown <kennethjiepadasas@gmail.com>
2018-11-04 20:58:36 +0800

    send sms function server

---
+++ local-sms-gateway/gateway.js
@@ -0,0 +1,42 @@
+const Nexmo = require('nexmo');
+const firebase = require('firebase');
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAO0KlzY_B4V5SuLIKHGA42a62TPcLaqTU",
+    authDomain: "blaze-5155c.firebaseapp.com",
+    databaseURL: "https://blaze-5155c.firebaseio.com",
+    projectId: "blaze-5155c",
+    storageBucket: "blaze-5155c.appspot.com",
+    messagingSenderId: "353744476046"
+  };
+  firebase.initializeApp(config);
+
+  const db = firebase.firestore();
+  db.settings({timestampsInSnapshots: true})
+const nexmo = new Nexmo({
+  apiKey: 'dad9f461',
+  apiSecret: 'm767kWD0ZjT52ZJ2'
+});
+
+db.collection("sendSms")
+.onSnapshot(function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+        if (change.type === "modified") {
+            console.log("Modified city: ", change.doc.data());
+            const from = 'Nexmo';
+            const to =  Number(change.doc.data().number);
+            const text = change.doc.data().message;
+            nexmo.message.sendSms(from, to, text, (error, reply) => {
+                if(error) {
+                throw error;
+                } else if(reply.messages[0].status != '0') {
+                console.log(reply);
+                throw 'Nexmo returned back a non-zero status';
+                } else {
+                console.log(reply);
+                }
+            });
+        }
+    });
+});
+
